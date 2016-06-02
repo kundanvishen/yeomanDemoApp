9ff04f5c1b98f3f3100695676c3c6ed8cc3be633
kundansingh1 <macmini-client@MACMINI-Clients-Mac-mini.local>
2016-06-02 10:56:53 +0530

    Change authentication

--- config/env/development.js
+++ config/env/development.js
@@ -4,10 +4,10 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'mongodb://<meanuser>:<meanpass>@ds021182.mlab.com:21182/kundan979') + '/mean-dev',
     options: {
-      user: '',
-      pass: ''
+      user: 'meanuser',
+      pass: 'meanpass'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
